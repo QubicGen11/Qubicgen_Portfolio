d76ee34b921e8c1e3cf43cb21f64a54793e2b9f7
Mahammed Sarfaraz Nawaz Pagala <pmdnawaz1@gmail.com>
2024-03-06 00:11:58 +0530

    seo optimization

--- src/SEO.jsx
+++ src/SEO.jsx
@@ -7,20 +7,22 @@ const SEO = ({ title, description, type, name, image, url, keywords }) => {
       {/* Standard metadata tags */}
       <title>{title}</title>
       <meta name='description' content={description} />
-
-      <meta
-        name="keywords"
-        content={keywords}
-        />
+      <meta name='keywords' content={keywords} />
       
-      {/* Facebook tags */}
+      {/* Open Graph tags */}
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={name} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:updated_time" content="2024-03-05T16:00:00Z" />
       
-      {/* Twitter tags */}
+      {/* Twitter card tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={name} />
       <meta name="twitter:title" content={title} />
@@ -29,7 +31,22 @@ const SEO = ({ title, description, type, name, image, url, keywords }) => {
       <meta name="twitter:url" content={url} />
       
       {/* Additional tags */}
-      {/* Add your additional tags here */}
+      <link rel="canonical" href={url} />
+      <meta name="robots" content="index, follow" />
+      <meta name="author" content={name} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      
+      {/* Additional tags inspired by powerful websites */}
+      <meta name="theme-color" content="#000000" /> {/* Define the theme color */}
+      <meta name="apple-mobile-web-app-capable" content="yes" /> {/* Allow full screen in iOS */}
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> {/* Define the status bar style in iOS */}
+      <meta name="application-name" content={name} /> {/* Define the name of the web application */}
+      <meta name="msapplication-TileColor" content="#000000" /> {/* Define the color of the Windows tile */}
+      <meta name="msapplication-config" content="/browserconfig.xml" /> {/* Define the path to the browser configuration file */}
+      <meta name="format-detection" content="email=no,telephone=no,address=no" /> {/* Disable auto detection */}
     </Helmet>
   );
 };
