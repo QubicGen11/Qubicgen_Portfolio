c37305a26f1893b7c7548d4a8cc4815adea084ad
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-03-08 21:06:57 +0530

    Alligned all text

--- src/Outsourcing Components/Outsourcinghead.jsx
+++ src/Outsourcing Components/Outsourcinghead.jsx
@@ -4,14 +4,18 @@ import 'aos/dist/aos.css';
 const Outsourcinghead = () => {
   return (
     <>
-       <div className="outsourcinghead" data-aos="fade-right">
 
-<h1 id='outsourcingtit'>
-Outsource for Success: Let Us Handle the Details While You Drive Growth.</h1>
-<p id='outsourcingdesc'style={{marginTop:'10px'}}>
-Outsource with Confidence: Partnering for Success in Every Endeavor. Let Us Handle the Workload, While You Focus on Your Vision.</p>
 
- </div>    
+ <div className="outsourcinghead d-flex align-items-center justify-content-start" data-aos="fade-right">
+    <div className="text-start mx-5  mb-24">
+        <h1 id="outsourcingtit"  >
+        Outsource for Success: Let Us Handle the Details While You Drive Growth.</h1>
+        <p id="outsourcingdesc" className='my-24  ' style={{width:'70vw'}}>
+        Outsource with Confidence: Partnering for Success in Every Endeavor. Let Us Handle the Workload, While You Focus on Your Vision.</p>
+    </div>
+    </div>
+
+ 
     
     </>
   )
