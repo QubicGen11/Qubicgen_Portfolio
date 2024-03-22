7a24fc2b166b2ef125dc318c7f72f864e0f2191b
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-03-22 17:33:29 +0530

    Updated the code of parallaxx

---
+++ src/Career jobs Components/Careerjobstext.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Careerjobstext = () => {
+  return (
+    <div className="careersjobtext mt-6 ">
+    <p className='text-xs sm:text-xl md:text-2xl lg:text-4xl xl:text-4xl mx-5 font-medium '  style={{color:'gold'}}>Explore exciting career opportunities at QubicGen. 
+  Discover roles that match your skills, passion, and ambition. 
+  Join our dynamic team and make a difference!</p>
+
+
+  <p className='text-xs sm:text-xl md:text-xl lg:text-2xl xl:text-2xl mx-5 text-white mt-4 font-normal'>
+  Welcome to QubicGen's career hub, your gateway to exciting opportunities! 
+Dive into a diverse array of job listings curated to match your skills and ambitions. 
+Whether you're a seasoned professional or just starting out, QubicGen offers positions that 
+promise growth, challenge, and fulfillment. Join our innovative and supportive team 
+environment, where you can shape the future while unlocking your potential. 
+Explore, apply, and begin your rewarding journey with QubicGen today.
+  </p>
+    
+</div>
+  )
+}
+
+export default Careerjobstext
