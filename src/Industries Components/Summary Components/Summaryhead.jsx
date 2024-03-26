4359d6d9b1b470eccaf805f2cd1f47d52c07b61a
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-03-26 17:08:44 +0530

    Updated some minor bugs

--- src/Industries Components/Summary Components/Summaryhead.jsx
+++ src/Industries Components/Summary Components/Summaryhead.jsx
@@ -8,7 +8,7 @@ const Summaryhead = () => {
   <div className="summaryhead d-flex align-items-center justify-content-start" data-aos="fade-right">
     <div className="text-start mx-5 ">
         <h1 id="summarytit">
-        Professional Summary    </h1>
+        Professional Services    </h1>
         <p id="summarydesc" className='my-14 ' style={{width:'70vw'}}>
         Delivering expert professional services and consulting solutions tailored to your business needs. Partner with us for unparalleled guidance and strategic insights.
         </p>
