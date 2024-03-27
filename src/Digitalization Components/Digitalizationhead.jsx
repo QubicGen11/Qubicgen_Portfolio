66c39c1d310d4959f9f869dcf5ed45ffcca9871d
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-03-27 14:12:07 +0530

    Updated some bugs "

--- src/Digitalization Components/Digitalizationhead.jsx
+++ src/Digitalization Components/Digitalizationhead.jsx
@@ -9,10 +9,11 @@ const Digitalizationhead = () => {
  <div className="digitalizationhead d-flex align-items-center justify-content-start" data-aos="fade-right">
     <div className="text-start mx-5 mb-24 ">
         <h1 id="digitalizationtit"  >
-        Ignite Your Brand's Online Presence: Unleash the Power of Digital Marketing!</h1>
+        Embrace the Digital Revolution: Transform Your Business with Our Digitalization Services!</h1>
         <p id="digitalizationdesc" className='my-24  ' style={{width:'70vw'}}>
-        Step into the spotlight with our dynamic digital marketing strategies. From captivating content to strategic campaigns, we'll propel your brand to new heights in the online arena.</p>
-    </div>
+        Explore new horizons with our digitalization services, where innovation meets efficiency to reshape your business landscape, streamline operations, and drive success in the digital age.
+        </p>
+            </div>
     </div>
     
     </>
