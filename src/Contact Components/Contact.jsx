7eee481c7e7702e3f6fed01ce396709e70ec6ea9
Mahammed Sarfaraz Nawaz Pagala <pmdnawaz1@gmail.com>
2024-03-28 00:11:15 +0530

    update contact form

--- src/Contact Components/Contact.jsx
+++ src/Contact Components/Contact.jsx
@@ -1,316 +1,242 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React from "react";
+import { useState } from "react";
+import Navbar from "../HomeComponents/Navbar";
+import Footer from "../HomeComponents/Footer";
 import axios from 'axios';
-import "./Contact.css"
-import Navbar from '../HomeComponents/Navbar'
-import Footer from '../HomeComponents/Footer'
-import Typewriter from 'typewriter-effect';
-import AOS from 'aos';
-import 'intl-tel-input/build/css/intlTelInput.css';
-import intlTelInput from 'intl-tel-input';
-import SEO from '../SEO';
-
-
 
 const Contact = () => {
+  const [activeTab, setActiveTab] = useState('student');
   const [isLoading, setIsLoading] = useState(false);
-  const containeroneRef = useRef(null);
-  const mobileCodeInputRef1 = useRef(null); // Ref for first input field
-  const mobileCodeInputRef2 = useRef(null); //
-
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
+    fullName: '',
     email: '',
-    jobTitle: '',
-    company: '',
     phone: '',
-    address: ''
+    details: '',
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
-        ...formData,
-        [name]: value
+      ...formData,
+      [name]: value
     });
-};
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsLoading(true);
     try {
+      formData.type = activeTab;
+      setIsLoading(true);
       const response = await axios.post('http://77.37.45.21:3000/api/contact', formData);
       console.log('Form data submitted:', response.data);
-      // Reset form after successful submission if needed
+      alert('Your message has been sent successfully');
+      // Clear form after successful submission
       setFormData({
-       fullName: '',
-       phone: '',
+        fullName: '',
         email: '',
-        jobTitle: '',
-        course:'',
-        message: ''
+        phone: '',
+        details: '',
+        company: '',
       });
-      alert('Your message has been sent successfully')
       setIsLoading(false);
+      window.location.reload();
     } catch (error) {
       setIsLoading(false);
-      alert('Something went wrong')
       console.error('Error submitting form:', error);
+      alert('Something went wrong');
     }
   };
-
-  useEffect(() => {
-    const sign_in_btn = document.querySelector("#sign-in-btn");
-    const sign_up_btn = document.querySelector("#sign-up-btn");
-
-    sign_up_btn.addEventListener("click", () => {
-      containeroneRef.current.classList.add("sign-up-mode");
-    });
-
-    sign_in_btn.addEventListener("click", () => {
-      containeroneRef.current.classList.remove("sign-up-mode");
-    });
-  }, []);
-
-  useEffect(() => {
-    const input1 = document.querySelector("#mobile_code1");
-    const iti1 = intlTelInput(input1, {
-      initialCountry: "in",
-      separateDialCode: true
-    });
-    mobileCodeInputRef1.current = iti1;
-
-    const input2 = document.querySelector("#mobile_code2");
-    const iti2 = intlTelInput(input2, {
-      initialCountry: "in",
-      separateDialCode: true
-    });
-    mobileCodeInputRef2.current = iti2;
-
-    return () => {
-      // Clean up the plugin when the component unmounts
-      iti1.destroy();
-      iti2.destroy();
-    };
-  }, []); 
-
   return (
     <>
-      <SEO
-        title="QubiGen - Contact Us"
-        description="Connect with QubiGen and get in touch with our team. Learn how you can reach out to us for inquiries, collaborations, and partnerships. We are here to support your business needs."
-        type="website"
-        name="QubiGen Contact"
-        image="https://example.com/contact-og-image.jpg"
-        url="https://www.qubicgen.com/contact"
-        keywords="QubiGen, Contact Us, inquiries, collaborations, partnerships, support"
-      />
-
-      <Navbar />
-      <div className="boxy" data-aos="fade-right">
-        <div className="containerone" ref={containeroneRef}>
-          <div className="forms-container">
-            <div className="signin-signup">
-
-              <form onSubmit={handleSubmit} className="sign-in-form">
-
-
-                <h2 className="text-3xl font-bold mt-4 md:mt-0 text-white" id='wearequbic' style={{ fontSize: '40px' }}>
-                  <Typewriter
-                    options={{
-                      autoStart: true,
-                      loop: true,
-
-                      delay: 2,
-                      strings: ['Project<span class="text-[#ffd700]"> Form</span>'],
-                      onComplete: (self) => {
-                        const textElement = self.el;
-                        const html = textElement.innerHTML;
-                        textElement.innerHTML = html.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-                      },
-                    }}
-                  />
+    <Navbar/>
+      <section className="relative bg-[#0a0a0a] overflow-hidden lg:p-8">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap lg:justify-between">
+            <div className="w-full px-4 lg:w-1/2 xl:w-6/12">
+              <div className="mb-12 max-w-[570px] lg:mb-0">
+                <span className="mb-4 font-bold block text-2xl text-primary">
+                  Contact Us
+                </span>
+                <h2 className="mb-6 font-semibold text-xl uppercase text-white">
+                  GET IN TOUCH WITH US
                 </h2>
-                <div className="input-field">
-                  <i className="fas fa-user" />
-                  <input type="text" placeholder="Enter Full Name" name="fullName" onChange={handleChange} value={formData.fullName} />
-                </div>
-
-                <div className="input-field">
-                  <i className="fas fa-envelope" />
-                  <input type="email" placeholder="Email" name="email" onChange={handleChange} value={formData.email} />
-                </div>
-                <div className="form-group">
-        <input
-          type="text"
-          id="mobile_code1"
-          className="input-field form-control"
-          placeholder="Phone Number"
-          name="phone"
-          ref={mobileCodeInputRef1}
-          onChange={handleChange}
-          value={formData.phone}
-        />
-      </div>
-                <div className="input-field">
-                  <i className="fas fa-briefcase" />
-                  <input type="text" placeholder="Job title" name="jobTitle" onChange={handleChange} value={formData.jobTitle} />
-                </div>
-                <div className="input-field">
-                  <i className="fas fa-building" />
-                  <input type="text" placeholder="Enter Company" name="course" onChange={handleChange} value={formData.course} />
-                </div>
-                <div className="input-field" style={{ height: '20vh' }}>
-                  <i className="fas fa-comment  flex justify-center items-center"  />
-                  <input type="text" placeholder="Comments" name="message" onChange={handleChange} value={formData.message}/>
-                </div>
-
-
-
-
-                <button type="submit" className="btn-53" style={{ marginTop: '20px' }} disabled={isLoading}>
-                  <div className="original"> {isLoading ? 'Submitting...' : 'Submit'}</div>
-                  <div className="letters">
-                    
-                      isLoading ? (
-                        <span>S</span>
-                        <span>U</span>
-                    <span>B</span>
-                    <span>M</span>
-                    <span>I</span>
-                    <span>T</span>
-                    <span>I</span>
-                    <span>N</span>
-                    <span>G</span>
-                    <span>.</span>
-                    <span>.</span>
-                      ) : (
-                        <span>S</span>
-                        <span>U</span>
-                    <span>B</span>
-                    <span>M</span>
-                    <span>I</span>
-                    <span>T</span>
-                      )
-                    
+                <p className="mb-9 text-base leading-relaxed text-white">
+                 Either you are an organization or a student, we are here to help.
+                </p>
+                <div className="mb-8 flex w-full max-w-[370px]">
+                  <div className="mr-6 flex h-[60px] w-full max-w-[60px] items-center justify-center overflow-hidden rounded bg-primary/5 text-primary sm:h-[70px] sm:max-w-[70px]">
                   </div>
-                </button>
-
-              </form>
-
-
-              <form onSubmit={handleSubmit} className="sign-up-form">
-                <h2 className="text-3xl font-bold mt-4 md:mt-0 text-white" id='wearequbic' style={{ fontSize: '40px' }}>
-                  <Typewriter
-                    options={{
-                      autoStart: true,
-                      loop: true,
-
-                      delay: 2,
-                      strings: ['Student<span class="text-[#ffd700]"> Form</span>'],
-                      onComplete: (self) => {
-                        const textElement = self.el;
-                        const html = textElement.innerHTML;
-                        textElement.innerHTML = html.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-                      },
-                    }}
-                  />
-                </h2>            <div className="input-field">
-                  <i className="fas fa-user" />
-                  <input type="text" placeholder="Enter Full Name" name="fullName" onChange={handleChange} value={formData.fullName} />
-                </div>
-
-
-                <div className="form-group">
-        <input
-          type="text"
-          id="mobile_code2"
-          className="input-field form-control"
-          placeholder="Phone Number"
-          name="phone"
-          ref={mobileCodeInputRef2}
-          onChange={handleChange}
-          value={formData.phone}
-        />
-      </div>
-
-
-                <div className="input-field">
-                  <i className="fas fa-envelope" />
-                  <input type="email" placeholder="Enter email" name="email" onChange={handleChange} value={formData.email} />
-                </div>
-                <div className="input-field">
-                  <i className="fas fa-check" />
-                  <select name="year" id="year" style={{ color: 'black' }} onChange={handleChange} value={formData.course}>
-                    <option value="select">Select a Course</option>
-                    <option value="rpa">RPA</option>
-                    <option value="year2">Web Development</option>
-                    <option value="year2">SAP</option>
-                    <option value="year2">Testing</option>
-                  </select>
-                </div>
-
-                <div className="input-field" style={{ height: '20vh' }}>
-                  <i className="fas fa-comment flex justify-center items-center"  />
-                  <input type="text" placeholder="Comments" name="message" onChange={handleChange} value={formData.message}/>
-                </div>
-
-                <button className="btn-53" style={{ marginTop: '10px' }}>
-                  <div className="original" >Submit</div>
-                  <div className="letters">
-
-                    <span>S</span>
-                    <span>U</span>
-                    <span>B</span>
-                    <span>M</span>
-                    <span>I</span>
-                    <span>T</span>
+                  <div className="w-full">
+                    <h4 className="mb-1 text-xl font-bold text-white ">
+                      Our Location
+                    </h4>
+                    <p className="text-base text-white">
+                      Anantapur, Andhra Pradesh
+                    </p>
                   </div>
-                </button>
+                </div>
 
+                <div className="mb-8 flex w-full max-w-[370px]">
+                  <div className="w-full">
+                    <h4 className="mb-1 text-xl font-bold text-white">
+                      Phone Number
+                    </h4>
+                    <p className="text-base text-white">
+                      (+62)81 414 257 9980
+                    </p>
+                  </div>
+                </div>
 
-
-              </form>
-
-
-
-            </div>
-          </div>
-
-
-
-
-
-          <div className="panels-container">
-            <div className="panel left-panel">
-              <div className="content" id='lookingproject'>
-              <h3 style={{ fontSize: '45px' }}>Want to Learn Skills?</h3>
-
-                <button className="btnone" id="sign-up-btn" style={{ width: '170px', position: 'relative', left: '76px', height: '50px', marginTop: '30px' }}>
-                  Student Form
-                </button>
-
-
+                <div className="mb-8 flex w-full max-w-[370px]">
+                  <div className="mr-6 flex h-[60px] w-full max-w-[60px] items-center justify-center overflow-hidden rounded bg-primary/5 text-primary sm:h-[70px] sm:max-w-[70px]">
+                  </div>
+                  <div className="w-full">
+                    <h4 className="mb-1 text-xl font-bold text-white ">
+                      Email Address
+                    </h4>
+                    <p className="text-base text-white">
+                      info@yourdomain.com
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="panel right-panel" style={{ position: 'relative', left: '-150px' }}>
-              <div className="content" id='studentskill'>
-                <h3 style={{ fontSize: '45px' }} id=''>Looking for Project ?</h3>
-
-
-                <button className="btnone" id="sign-in-btn" style={{ width: '170px', position: 'relative', left: '140px', height: '50px' }}>
-                  Project Form
-                </button>
+            <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
+            <div className="mb-4 flex border-b">
+              <button
+                className={`mr-4 pb-2 text-lg font-semibold ${activeTab === 'student' ? 'border-b-2 border-primary text-primary' : 'text-white'}`}
+                onClick={() => setActiveTab('student')}
+              >
+                Student
+              </button>
+              <button
+                className={`pb-2 text-lg font-semibold ${activeTab === 'company' ? 'border-b-2 border-primary text-primary' : 'text-white'}`}
+                onClick={() => setActiveTab('company')}
+              >
+                Company
+              </button>
+            </div>
+              <div className="relative rounded-lg p-8 shadow-lg sm:p-12">
+              <form onSubmit={handleSubmit}>
+      {activeTab === 'student' ? (
+        <>
+          <input
+           className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            type="text"
+            name="fullName"
+            placeholder="Your Name"
+            value={formData.fullName}
+            onChange={handleChange}
+          />
+          <input
+           className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            type="text"
+            name="email"
+            placeholder="Your Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+          className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            type="text"
+            name="phone"
+            placeholder="Your Phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+          <input
+          className="w-full rounded border mt-8 border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            placeholder="Your Request"
+            name="details"
+            value={formData.details}
+            onChange={handleChange}
+          />
+        </>
+      ) : (
+        <>
+          <input
+           className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            type="text"
+            name="fullName"
+            placeholder="Your Company's Name"
+            value={formData.fullName}
+            onChange={handleChange}
+          />
+          <input
+           className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            type="text"
+            name="email"
+            placeholder="Your Company's Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+          className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            type="text"
+            name="phone"
+            placeholder="Your Company's Phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+          <input
+          className="w-full rounded border mt-8 border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+            placeholder="Your Request"
+            name="details"
+            value={formData.details}
+            onChange={handleChange}
+          />
+        </>
+      )}
+      <div>
+        <button
+          type="submit"
+          className="w-full mt-8 rounded border border-primary bg-yellow p-3 text-white transition hover:bg-opacity-90"
+        > {isLoading ? 'Sending...' :
+          'Send Message'}
+        </button>
+      </div>
+    </form>
+                <div>
+                </div>
               </div>
             </div>
           </div>
-
         </div>
-      </div>
-      <Footer />
+      </section>
+    <Footer />
     </>
+  );
+};
 
-  )
-}
+export default Contact;
 
-export default Contact
+// const input = ({ row, placeholder, name, defaultValue }) => {
+//   return (
+//     <>
+//       <div className="mb-6">
+//         <textarea
+//           rows={row}
+//           placeholder={placeholder}
+//           name={name}
+//           className="w-full resize-none rounded  mt-8 border border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+//           defaultValue={defaultValue}
+//         />
+//       </div>
+//     </>
+//   );
+// };
+
+// const input = ({ type, placeholder, name }) => {
+//   return (
+//     <>
+//       <div className="mb-6">
+//         <input
+//           type={type}
+//           placeholder={placeholder}
+//           name={name}
+//           className="w-full rounded border mt-8 border-stroke px-[14px] py-3 text-base text-black outline-none focus:border-primary"
+//         />
+//       </div>
+//     </>
+//   );
+// };
