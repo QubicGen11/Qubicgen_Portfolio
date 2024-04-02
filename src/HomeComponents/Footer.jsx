9a5408c4858b1b429338738011a17f7a03561d88
Shaik Sajid Hussain <194G1A0291@Srit.ac.in>
2024-04-03 04:52:01 +0530

    updated the mail in footer

--- src/HomeComponents/Footer.jsx
+++ src/HomeComponents/Footer.jsx
@@ -191,7 +191,7 @@ const Footer = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="size-5 shrink-0 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
-                    <p className="flex-1  dark:text-gray-300 "><a href="mailto:someone@example.com"> services@qubicgen.com </a></p>
+                    <p className="flex-1  dark:text-gray-300 "><a href="mailto:services@qubicgen.com"> services@qubicgen.com </a></p>
                   </a>
                 </li>
                 <li>
