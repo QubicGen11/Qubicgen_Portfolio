77d550f85c9a1d33650073c8a9cbd086e69d15e9
shaiksajidhussain <sanjusazid0@gmail.com>
2024-12-06 17:14:54 +0530

    added teh test one

--- src/Courses Contacts/Course Admin Component/Courseadmin.jsx
+++ src/Courses Contacts/Course Admin Component/Courseadmin.jsx
@@ -558,7 +558,8 @@ const CreateCourse = () => {
   };
  
   return (
-    <div className="min-h-screen bg-cover bg-center">
+
+      <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1665686308827-eb62e4f6604d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
       <div className="bg-black bg-opacity-50 min-h-screen p-8">
         <div className="max-w-4xl mx-auto bg-gray-900 p-8 rounded-lg shadow-lg">
           <form onSubmit={handleSubmit} className="space-y-6">
