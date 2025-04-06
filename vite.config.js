11bf4a64ad5653152245709cf995572e05c4a8ab
venky nagidi <venky@qubicgen.com>
2025-04-06 19:16:51 +0530

    deployment files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 	plugins: [react()],
+        server: {
+       	 host: "0.0.0.0",
+         port: 8086
+  	}
 	// base: ['https://www.qubicgen.com/', 'https://qubicgen.com/'],
 });
